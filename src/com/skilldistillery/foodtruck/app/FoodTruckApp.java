009771174050f189af91cd76b3b5da1738ff9b2c
xanimal37 <hawkbit37@gmail.com>
2023-01-21 14:45:22 -0600

    skeleton code

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,17 +1,66 @@
 package com.skilldistillery.foodtruck.app;
 
+import java.util.Scanner;
 import com.skilldistillery.foodtruck.entities.FoodTruck;
 
 public class FoodTruckApp {
 
 	//fleet of FoodTrucks
 	private FoodTruck[] fleet;
+	Scanner input;
+	//variable to test if we quit the program
+	boolean keepGoing;
+	//track number of trucks in fleet
+	int numTrucks;
 	
 	public static void main(String[] args) {
 		
+		FoodTruckApp foodTruckApp = new FoodTruckApp();
 		
+	}
 	
-
+	public FoodTruckApp() {
+		
+		//instantiate array big enough to hold 5 trucks
+		fleet = new FoodTruck[5];
+		//Instantiate a scanner to use for all input
+		input = new Scanner(System.in);
+		keepGoing = true;
+		numTrucks=0;
+		
+	}
+	
+	//method to get initial user choice (enter truck or quit)
+	void chooseOption(Scanner scanner) {
+		System.out.println("Would you like to enter a food truck? (Y or QUIT)");
+		String appChoice = scanner.nextLine();
+		appChoice = appChoice.toUpperCase();
+		switch(appChoice) {
+		case "Y":
+			System.out.println("Not implemented yet");
+			break;
+		case "QUIT":
+			closeApp();
+			break;
+		default:
+			System.out.println("Not implemented yet. need to loop input");
+			break;
+		}
+	}
+	
+	//method to close app
+	void closeApp() {
+		System.out.println("Goodbye!");
+		System.exit(0);
+	}
+	
+	//method to get foodtruck info
+	void getTruckInfo(Scanner scanner) {
+		//check for space in fleet
+		
+		//instantiate a new truck
+		FoodTruck newTruck = new FoodTruck();
+		System.out.println("Enter the name of the food truck:");
 	}
 
 }
--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -3,5 +3,43 @@ package com.skilldistillery.foodtruck.entities;
 public class FoodTruck {
 
 	private static int nextTruckId;
+	
 	private int id;
+	private String name;
+	private String foodType;
+	
+	//constructor
+	//auto-assign id using static variable owned by FoodTruck class
+	public FoodTruck() {
+		id=nextTruckId++;
+	}
+	
+	//setters
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setfoodType(String foodType) {
+		this.foodType = foodType;
+	}
+	
+	//getters
+	public String getName() {
+		return name;
+	}
+	
+	public String getFoodType() {
+		return foodType;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	
+	//override object string method (good practice)
+	@Override
+	public String toString() {
+		return "not implemented yet";
+	}
 }
